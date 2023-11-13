0869fc171ad8686a18f9326d291f72214a3af1e9
KG <khatunaggg12345@gmail.com>
2023-11-13 11:54:50 +0400

    myTask-01

---
+++ script.js
@@ -0,0 +1,74 @@
+
+// ------1------
+console.log('Hello World');
+
+
+// ------2------
+let num = 11;
+console.log(num);
+
+// ------3------
+console.log(133 + 46);
+
+
+
+let a = 133;
+let b = 46;
+console.log(a + b);
+
+// ------4------
+console.log(17 - 3)
+
+let c = 17;
+let d = 3;
+console.log(c - d);
+
+
+// ------5------
+console.log(2 * 2);
+
+let a2 = 2;
+let b2 = 2
+
+console.log(a2 * b2);
+
+
+
+// ------6------
+console.log(10 / 5);
+
+let a3 = 10;
+let b3 = 5;
+console.log(a3 / b3);
+
+
+
+// ------7------
+console.log(9 % 4)
+
+let a4 = 9;
+let b4 = 4;
+console.log(a4 % b4); 
+
+
+
+// ------8------
+let str = 'Hello World';
+console.log(str.length);
+
+
+// ------9------
+console.log('This is my ' + 'first lesson');
+
+let str3 = 'This is my';
+let str4 = 'first lesson';
+console.log(str3 + ' ' + str4);
+
+// ------10------
+
+let str2 = 'This is my first lesson';
+console.log(str2.toUpperCase());
+
+
+// ------11------
+console.log(str2.toLowerCase());
